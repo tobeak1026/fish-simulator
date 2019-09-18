5896ce1ee44854a5a43e157c6608adaaefd6721d
tobeak1026 <tobeak1026@vip.163.com>
2019-09-19 07:51:41 +0800

    add fish

--- bin/test1.js
+++ bin/test1.js
@@ -1,14 +1,38 @@
 const Simulator = require('../index');
 const {vec2} = require('gl-matrix');
+const util = require('../lib/util');
 
 
 let sim = new Simulator();
 sim.startup();
 sim.comp('bullet').schedule(0, {});
+sim.comp('fish').schedule(0, {
+    bodies: [
+        util.rectPoints(10, 50),
+        util.rectPoints(10, 50),
+    ],
+    path: [
+        [0, 0],
+        [1, 1],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+    ]
+});
 sim.frame();
 
-let b1 = sim.comp('bullet').getBullet(0);
-b1.setRotation(Math.PI / 4);
+// let b1 = sim.comp('bullet').getBullet(0);
+// b1.setRotation(Math.PI / 4);
+
+// let f1 = sim.comp('fish').getFish(0);
+// console.log(f1.getPosition());
+
+
+sim.frame();
+sim.frame();
+
+// console.log(f1.getPosition(), f1.getRotation());
 
 // console.log(b1.getRotation());
 // console.log(b1.getVelocity());
