15027c783d3f1b9b2b038da89abbbacf14d44a66
tobeak1026 <tobeak1026@vip.163.com>
2019-09-18 12:42:44 +0800

    fix bug

--- bin/test1.js
+++ bin/test1.js
@@ -1,8 +1,55 @@
 const Simulator = require('../index');
+const {vec2} = require('gl-matrix');
 
 
 let sim = new Simulator();
 sim.startup();
+sim.comp('bullet').schedule(0, {});
 sim.frame();
-sim.frame();
-console.log(sim);
+
+let b1 = sim.comp('bullet').getBullet(0);
+b1.setRotation(Math.PI / 4);
+
+// console.log(b1.getRotation());
+// console.log(b1.getVelocity());
+// 
+// let v1 = vec2.create();
+// vec2.rotate(v1, vec2.fromValues(1, 0), vec2.fromValues(0, 0), Math.PI);
+// console.log(v1);
+// 
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// console.log(b1.getPosition());
+// sim.frame();
+// sim.frame();
+// sim.frame();
+// 
+// console.log(b1.getPosition());
+// sim.frame();
+// sim.frame();
+// sim.comp('bullet').schedule(0, {});
+// sim.comp('bullet').schedule(0, {});
+// sim.frame();
+// sim.frame();
+// console.log(sim);
